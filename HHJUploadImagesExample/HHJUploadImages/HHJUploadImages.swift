61e0a30d1991c5ce42f39ab50c32c76d77482db2
szulmj <szhonghaijiang@163.com>
2016-05-20 16:27:34 +0800

    add HHJUploadImagesExample

---
+++ HHJUploadImagesExample/HHJUploadImages/HHJUploadImages.swift
@@ -0,0 +1,102 @@
+//
+//  HHJUplodaImages.swift
+//  HHJUplodaImagesExample
+//
+//  Created by szulm on 16/5/20.
+//  Copyright © 2016年 HM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public typealias HHJUploadImagesBlock = (data: NSData?, response: NSURLResponse?, error: NSError?, index: Int) -> Void
+public class HHJUploadImage {
+    
+    /**
+     //公开一个类方法用来上传图片
+     
+     - parameter images:    图片数组
+     - parameter stringUrl: 地址
+     - parameter params:    参数
+     */
+    public static func uploadimages(images:[UIImage], stringUrl: String, params: [String: String], blocK: HHJUploadImagesBlock?) {
+        HHJUploadImage.hhjUploadImageInstance.uploadimages(images, stringUrl: stringUrl, params: params, blocK: blocK)
+    }
+    
+    
+    
+    static let hhjUploadImageInstance = HHJUploadImage()
+    /**
+     上传多张图片
+     
+     - parameter images:    图片数组
+     - parameter stringUrl: 地址
+     - parameter params:    参数
+     - parameter blocK:     回调Block
+     */
+    var currentImageIndex = 0
+    var images: [UIImage]!
+    var block: HHJUploadImagesBlock?
+    func uploadimages(images:[UIImage], stringUrl: String, params: [String: String], blocK:HHJUploadImagesBlock?)  {
+        assert(images.count > 0, "上传的图片数组不能为空！")
+        currentImageIndex = 0
+        let image = images[currentImageIndex]
+        self.images = images
+        self.block = blocK
+        uploadimage(image, stringUrl: stringUrl, params: params)
+    }
+    
+    
+    // MARK-:上传单张图片
+    func uploadimage(image:UIImage, stringUrl: String, params: [String: String]) {
+        guard let url = NSURL(string: stringUrl) else {
+            assertionFailure("\(stringUrl) 无法生成URL")
+            return
+        }
+        
+        let request = NSMutableURLRequest(URL: url, cachePolicy: .ReloadIgnoringCacheData, timeoutInterval: 60)
+        request.HTTPMethod = "POST"
+        request.HTTPShouldHandleCookies = true
+        for (key, value) in params {
+            request.setValue(value, forHTTPHeaderField: key)
+        }
+        let boundary = "unique-consistent-string"
+        let contentType = String(format: "multipart/form-data; boundary=%@", boundary)
+        request.setValue(contentType, forHTTPHeaderField: "Content-Type")
+        let data = NSMutableData()
+        data.appendData(String(format: "--%@\r\n", boundary).dataUsingEncoding(NSUTF8StringEncoding)!)
+        data.appendData(String(format: "Content-Disposition: form-data; name=%@\r\n\r\n", "imageCaption").dataUsingEncoding(NSUTF8StringEncoding)!)
+        data.appendData(String(format: "%@\r\n", "Some Caption").dataUsingEncoding(NSUTF8StringEncoding)!)
+        
+        var imageData = UIImageJPEGRepresentation(image, 1)
+        if imageData == nil {
+            imageData = UIImagePNGRepresentation(image)
+        }
+        
+        if imageData != nil {
+            data.appendData(String(format: "--%@\r\n", boundary).dataUsingEncoding(NSUTF8StringEncoding)!)
+            data.appendData(String(format: "Content-Disposition: form-data; name=%@; filename=imageNamestory.jpg\r\n", "imageFormKey").dataUsingEncoding(NSUTF8StringEncoding)!)
+            data.appendData(String(format: "Content-Type: image/jpeg\r\n\r\n").dataUsingEncoding(NSUTF8StringEncoding)!)
+            data.appendData(imageData!)
+            data.appendData(String(format: "\r\n").dataUsingEncoding(NSUTF8StringEncoding)!)
+        }
+        data.appendData(String(format: "--%@--\r\n", boundary).dataUsingEncoding(NSUTF8StringEncoding)!)
+        request.HTTPBody = data
+        request.setValue(String(format: "%d", data.length), forHTTPHeaderField: "Content-Length")
+        
+        let session = NSURLSession.sharedSession()
+        
+        var dataTask: NSURLSessionUploadTask! = nil
+        dataTask = session.uploadTaskWithRequest(request, fromData: nil) { (data, response, error) -> Void in
+            if let tempBlock = self.block {
+                tempBlock(data: data, response: response, error: error, index: self.currentImageIndex)
+            }
+            self.currentImageIndex += 1
+            if self.currentImageIndex >= self.images.count { return }
+            let image = self.images[self.currentImageIndex]
+            self.uploadimage(image, stringUrl: stringUrl, params: params)
+        }
+        dataTask.resume()
+    }
+}
+
---
+++ HHJUploadImagesExample/HHJUploadImagesExample/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  HHJUploadImagesExample
+//
+//  Created by bu88 on 16/5/20.
+//  Copyright © 2016年 HM. All rights reserved.
+//
+
+import UIKit
+
+let sid = "sid"
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let iamge1 = UIImage(named: "pet_add")
+        let image2 = UIImage(named: "switchOff")
+        let image3 = UIImage(named: "switchOn")
+        
+        HHJUploadImage.uploadimages([iamge1!, image2!, image3!], stringUrl: "", params: ["sid": sid]) { (data, response, error, index) in
+            print("data?.length:\(data?.length)")
+            print("response:\(response)")
+            print("error:\(error)")
+            print("index:\(index)")
+        }
+    }
+}
+
